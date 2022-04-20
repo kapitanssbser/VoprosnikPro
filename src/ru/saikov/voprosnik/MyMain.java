6d0bb87234ca1ea40931e1fa8aac49775167c766
kapitanssbser <chasovshik.market@yandex.ru>
2022-04-20 21:27:17 +0300

    Добавил в класс VOPROS поля и сетеры с гетерами

--- src/ru/saikov/voprosnik/MyMain.java
+++ src/ru/saikov/voprosnik/MyMain.java
@@ -2,10 +2,5 @@ package ru.saikov.voprosnik;
 
 public class MyMain {
     public static void main(String[] args) {
-        System.out.println("Основной класс");
-        Vopros vopros = new Vopros();
-        vopros.test();
-        System.out.println("Что то добавили...");
-        System.out.println("Теперь и с ноутбука!");
     }
 }
--- src/ru/saikov/voprosnik/Vopros.java
+++ src/ru/saikov/voprosnik/Vopros.java
@@ -1,8 +1,57 @@
 package ru.saikov.voprosnik;
 
-public class Vopros {
-    public void test(){
-        System.out.println("eeeeee");
-        System.out.println("Add...");
+public class Vopros { //Класс вопрос
+    String vopros;//Сам вопрос
+    String otvet;//Ответ на вопрос
+    String variant1, variant2, variant3;//Варианты ответа
+    enum Gruppy {ELECTRIK, SPEC_SYST};//Группы
+    Gruppy gruppy;
+    //Сеттеры и геттеры
+    public void setVopros(String vopros) {
+        this.vopros = vopros;
+    }
+
+    public String getVopros() {
+        return vopros;
+    }
+
+    public void setOtvet(String otvet) {
+        this.otvet = otvet;
+    }
+
+    public String getOtvet() {
+        return otvet;
+    }
+
+    public void setVariant1(String variant1) {
+        this.variant1 = variant1;
+    }
+
+    public String getVariant1() {
+        return variant1;
+    }
+
+    public void setVariant2(String variant2) {
+        this.variant2 = variant2;
+    }
+
+    public String getVariant2() {
+        return variant2;
+    }
+
+    public void setVariant3(String variant3) {
+        this.variant3 = variant3;
+    }
+
+    public String getVariant3() {
+        return variant3;
+    }
+
+    public void setGruppy(Gruppy gruppy) {
+        this.gruppy = gruppy;
+    }
+
+    public Gruppy getGruppy() {
+        return gruppy;
     }
 }
