f997495570c93d78b9d153e302b4293cc37950f5
kapitanssbser <chasovshik.market@yandex.ru>
2022-04-20 22:58:00 +0300

    Добавил начальное меню

--- src/ru/saikov/voprosnik/MyMain.java
+++ src/ru/saikov/voprosnik/MyMain.java
@@ -2,6 +2,12 @@ package ru.saikov.voprosnik;
 
 public class MyMain {
     public static void main(String[] args) {
-
+        new MyMain().go();
     }
+    public void go() {
+        System.out.println("Программа для проверки знаний работников службы эксплуатации ООО Солар Кремниевые технологии");
+        System.out.println("1. Администратор.");
+        System.out.println("2. Пользователь.");
+    }
+
 }
