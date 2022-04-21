1f4e15973b024552dfc15d5cef4537f3aea0e70a
kapitanser <chasovshik.market@yandex.ru>
2022-04-21 17:38:24 +0300

    Добавил вложенное меню

--- src/ru/saikov/voprosnik/MyMain.java
+++ src/ru/saikov/voprosnik/MyMain.java
@@ -5,6 +5,9 @@ public class MyMain {
     public final String ITEMS1_2 = "2. Пользователь";
     public final String ITEMS1_3 = "3. Выход из программы";
 
+    public final String ITEMS2_1 = "1. Добавить пользователя в базу";
+    public final String ITEMS2_2 = "2. Назад";
+
     public static void main(String[] args) {
         new MyMain().go();
     }
@@ -17,6 +20,8 @@ public class MyMain {
             String mychosse = myMenu.getItemMenu();
             if (mychosse == ITEMS1_1) {
                 System.out.println("Выбран администратор");
+                go1();
+                myMenu.printMenu();
             }
             if (mychosse == ITEMS1_2) {
                 System.out.println("Выбран пользователь");
@@ -27,6 +32,22 @@ public class MyMain {
             }
             if((mychosse == "NC") || (mychosse == null)){
                 System.out.println("Введите корректный пункт меню");
+            }
+        }
+    }
+
+    public void go1(){
+        MyMenu myMenu = new MyMenu(ITEMS2_1,ITEMS2_2,null,null,null);
+        myMenu.printMenu();
+        boolean cykl = true;
+        while (cykl){
+            String myChosse = myMenu.getItemMenu();
+            if (myChosse == ITEMS2_1){
+                System.out.println("Выбран добавить пользователя в БД");
+            }
+            if (myChosse == ITEMS2_2) {
+                System.out.println("Выбрано назад");
+                break;
             }
         }
     }
