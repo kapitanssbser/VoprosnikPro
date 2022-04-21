b1726c3a4a484c8aab1b969fbda0d58bc21414a0
kapitanssbser <chasovshik.market@yandex.ru>
2022-04-21 21:03:04 +0300

    Добавил комментарии к MyMenu

--- src/ru/saikov/voprosnik/MyMenu.java
+++ src/ru/saikov/voprosnik/MyMenu.java
@@ -5,33 +5,33 @@ import java.util.Scanner;
 
 public class MyMenu {
     int chosse = 1;//Выбраный элемент меню
-    ArrayList<String> listMenu = new ArrayList<String>();
+    ArrayList<String> listMenu = new ArrayList<String>(); //Список для элементов меню
 
-    public MyMenu(String a_item1, String a_item2, String a_item3, String a_item4, String a_item5){
-        listMenu.add(a_item1);
-        listMenu.add(a_item2);
+    public MyMenu(String a_item1, String a_item2, String a_item3, String a_item4, String a_item5){  //Конструктор
+        listMenu.add(a_item1);   //Заполняем список элементами меню
+        listMenu.add(a_item2);   //Если пункт меню не нужен передаем null
         listMenu.add(a_item3);
         listMenu.add(a_item4);
         listMenu.add(a_item5);
     }
 
-    public void printMenu(){
-        for (String s : listMenu){
-            if (s != null) {
+    public void printMenu(){   //Печать меню
+        for (String s : listMenu){  //Проходим по всему списку
+            if (s != null) {   //Выводим на экран только НЕ ПУСТЫЕ элементы
                 System.out.println(s);
             }
         }
     }
 
-    public String getItemMenu(){
+    public String getItemMenu(){   //Метод возвращает что нажал пользователь
         String items;
         Scanner scanner = new Scanner(System.in);
-        int s = scanner.nextInt();
-        if (s == 1){
-            items = listMenu.get(s - 1);
-            return items;
+        int s = scanner.nextInt();   //Запрашиваем ввод
+        if (s == 1){   //При конкретном варианте
+            items = listMenu.get(s - 1); //Получаем соответствующий элемент из списка
+            return items;   //Возвращаем
         }
-        if (s == 2){
+        if (s == 2){      //Далее аналогично
             items = listMenu.get(s - 1);
             return items;
         }
@@ -48,6 +48,6 @@ public class MyMenu {
             return items;
         }
 
-        return "NC";
+        return "NC";  //Если не один вариант не прошел, нужно для проверки диапазон введенных пользователя
     }
 }
